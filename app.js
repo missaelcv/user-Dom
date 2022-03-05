945310765663a38b75214fd1ba0eb5c1ea36dfd9
missaelcv <missaelcv0220@gmail.com>
2022-03-04 23:31:34 -0400

    Metodo totalTime

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const app = new Vue({
     el: '#app',
-
     
-    data(){
+    data () {
+     return {
+        courses: [],
+       
+       title: '',
+       time: ''
+     }
+    },
+    
+    computed: {
+      totalTime () {
+        if (!this.courses.length) { return 0 }
         
+        return this.courses.reduce((a, b) => a + parseInt(b.time), 0)
+      }
+    },
+    
+    methods: {
+      addCourse () {
+        if (!this.title || !this.time) { return }
+        
+        this.courses.push({
+          title: this.title,
+          time: this.time
+        })
+        
+        this.title = ''
+        this.time = 0
+      }
     }
-})
+  })
